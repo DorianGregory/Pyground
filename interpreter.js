dd38fea2d3668c99334b1e5063fde514b64075e4
DorianGregory <dorian@Jellyfish.local>
2017-12-02 19:43:39 -0500

    Added interpreter

---
+++ interpreter.js
@@ -0,0 +1,26 @@
+class Line {
+	constructor(lineNumber, sourceText) {
+		this.lineNumber = lineNumber;
+		this.sourceText = sourceText;
+	}
+	varName() {
+
+	}
+	compVarName() {
+
+	}
+	changeLine(newSourceText) {
+		this.sourceText = 
+	}
+}
+
+class codeFile {
+	// makes a code file
+	// takes in an array of Lines
+	constructor(lines) {
+		this.lines = lines
+	}
+	modifyLine(lineNumber, line) {
+		
+	}
+}
